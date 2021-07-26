7f674a44f84f1982cad22db6129534862f7a3125
JiwonSon0103 <49024048+JiwonSon0103@users.noreply.github.com>
2021-07-26 13:34:04 +0900

    음양더하기-지원

---
+++ 월간 코드 챌린지 시즌1-두개뽑아서더하기-JiwonSon.js
@@ -0,0 +1,24 @@
+function solution(numbers) {
+    var answer = [];
+    
+    // 값 구하기 
+    const len = numbers.length;
+    for(var i=0; i<len; i++){
+        for(var j=i+1; j<len; j++)
+        answer.push(numbers[i] + numbers[j]);
+    }
+    
+    // 중복 제거
+    // 방법1)
+    answer = answer.filter((el,i)=>{
+        return answer.indexOf(el)===i;
+    });
+    // 방법2)
+    // const set = new Set(answer);
+    // answer = [...set];
+    
+    // 정렬
+    answer = answer.sort((a,b)=>a-b);
+    
+    return answer;
+}
