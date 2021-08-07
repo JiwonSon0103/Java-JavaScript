a6effc4bf4a895594be88a1717b3ef850d3f9f9d
Ywoosang <opellong13@gmail.com>
2021-08-08 01:09:56 +0900

    124 나라의 숫자-우상

--- Level2/연습문제-124 나라의 숫자-Ywoosang.js
+++ Level2/연습문제-124 나라의 숫자-Ywoosang.js
@@ -2,8 +2,7 @@ function solution(n) {
     const rec = (answer,num)=>{
         const value = (num - 1) % 3 == 0 ? 1 : (num - 1) % 3 == 1 ? 2 : 4;
         answer  = value.toString() + answer;
-        if(parseInt((num-1)/3)==0) return answer;
-        else return rec(answer,parseInt((num-1)/3));
+        return parseInt((num-1)/3)==0 ? answer: rec(answer,parseInt((num-1)/3));
     }
     return rec("",n); 
 }
